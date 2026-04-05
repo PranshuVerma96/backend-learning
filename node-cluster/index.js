a2634ad60dc9e525192265b1ac9c842c12bd3fae
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-04-05 12:02:32 +0530

    Add files via upload
    this is about cluster in node js

---
+++ node-cluster/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const PORT =  8000;
+
+app.get('/' , (req, res)=>{
+  return res.json({message : 'Hello from express server '});
+});
+
+app.listen(PORT , ()=>{console.log(`server Started At PORT : ${PORT}`);
+})

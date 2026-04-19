8459e60181b7284e2431acb9d1be6704b842a8ae
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-04-19 06:18:33 +0530

    Add files via upload

---
+++ Real_time_message_app/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const {Server} = require('socket.io');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.resolve('./public')));
+
+app.get('/' ,(req, res)=>{
+return res.sendFile('./public/index.html')
+})
+
+// socket .io
+
+io.on('connection' , (socket)=>{
+ socket.on('user-message',(message)=>{
+
+  io.emit("message" , message);
+ });
+  
+});
+
+
+server.listen(8900,()=>{console.log(`server started at 8900`);
+})
